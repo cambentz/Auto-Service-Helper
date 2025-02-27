a936cc521e1721d08c2f263b25f5ea65f110c9a6
alex-hnt <170971131+alex-hnt@users.noreply.github.com>
2025-02-26 19:03:24 -0500

    add guides POST route

--- Server/db/guideQueries.js
+++ Server/db/guideQueries.js
@@ -3,11 +3,23 @@ const { pool } = require('./index.js');
 async function getAllGuides() {
     const query = "SELECT * FROM guides";
     try {
-        let res = await pool.query(query);
-        return res.rows;
-    } catch (e) {
-        console.log(e);
+        let result = await pool.query(query);
+        return result.rows;
+    } catch (err) {
+        console.error(err);
+        return false;
     }
 }
 
-module.exports = { getAllGuides }
+async function createGuide(name, description, thumbnail) {
+    const query = "INSERT INTO guides (name, description, thumbnail) VALUES ($1, $2, $3) RETURNING *";
+    try {
+        let result = await pool.query(query, [name, description, thumbnail]);
+        return result.rows[0];
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+}
+
+module.exports = { getAllGuides, createGuide }
--- Server/routes/guideRoutes.js
+++ Server/routes/guideRoutes.js
@@ -7,10 +7,24 @@ router.get('/', async (req, res) => {
     let guides = await guideQueries.getAllGuides();
 
     if (!guides || guides.length === 0) {
-        return res.status(404).json({ error: 'No guides found.'});
+        return res.status(404).json({ error: 'No guides found'});
     }
 
     res.json(guides);
+});
+
+router.post('/', async (req, res) => {
+    const { name, description, thumbnail } = req.body;
+
+    if (!name || !name.trim()) return res.status(400).json({ error: 'Name not provided'});
+
+    let result = await guideQueries.createGuide(name, description, thumbnail);
+    if (result) {
+        return res.json(result);
+    }
+    else {
+        return res.status(500).json({ error: 'Error creating guide'})
+    }
 });
 
 module.exports = router;
