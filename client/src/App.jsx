a4e74baaf0d1b38c67536a8469786ad102802571
tjgibbard <tgibbard@umich.edu>
2025-04-13 15:52:11 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./Pages/Home";
 import Garage from "./Pages/Garage";
@@ -16,7 +16,6 @@ import { AuthProvider } from "./utils/AuthContext";
 const App = () => {
   return (
     <BrowserRouter>
-    <Router>
       <AuthProvider>
         <Routes>
           {/* Parent route with layout */}
@@ -27,16 +26,15 @@ const App = () => {
             <Route path="guides" element={<Guides />} />
             <Route path="guides/:guideId" element={<GuidePage />} />
             <Route path="help" element={<Help />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/auth/reset" element={<Auth />} />
+            <Route path="auth" element={<Auth />} />
+            <Route path="auth/reset" element={<Auth />} />
             <Route path="reset-password" element={<ResetPassword />} />
             <Route path="settings" element={<Settings />} />
-            <Route path="/add-vehicle" element={<AddVehiclePage />} />
+            <Route path="add-vehicle" element={<AddVehiclePage />} />
             <Route path="*" element={<ErrorPage />} />
           </Route>
         </Routes>
       </AuthProvider>
-    </Router>
     </BrowserRouter>
   );
 };
